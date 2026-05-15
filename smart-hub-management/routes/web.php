04815b45837413ee9a4c4d80b69fb5be40959192
Baymaxxxu <411212105@mahasiswa.undira.ac.id>
2026-05-15 21:39:30 +0700

    feat: add admin web routes

--- smart-hub-management/routes/web.php
+++ smart-hub-management/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\DashboardController;
+use App\Http\Controllers\Web\EquipmentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/web/equipment', [EquipmentController::class, 'index']);
+    Route::get('/web/equipment/create', [EquipmentController::class, 'create']);
+    Route::post('/web/equipment', [EquipmentController::class, 'store']);
+    Route::get('/web/equipment/{id}/edit', [EquipmentController::class, 'edit']);
+    Route::put('/web/equipment/{id}', [EquipmentController::class, 'update']);
+    Route::delete('/web/equipment/{id}', [EquipmentController::class, 'destroy']);
 });
